1e42302108acfad055936428aa37cc5c8c39ef18
Alexandre Nicolas <alex29820@gmail.com>
2018-02-15 12:25:41 +0100

    Align calendar correctly

--- src/enseignant/home_page/HomePage.js
+++ src/enseignant/home_page/HomePage.js
@@ -2,23 +2,42 @@ import React from 'react'
 import { DatePicker, Button, Icon } from 'antd';
 import moment from 'moment';
 
+import './HomePage.css';
+
 const dateFormat = 'YYYY/MM/DD';
 const monthFormat = 'YYYY/MM';
 
+const styles = {
+  buttonGroup: {
+    display: 'flex'
+  },
+  button: {
+    width: '100%'
+  }
+}
+
 const HomePage = (props) => (
-    <div>
-            <div className="Calendar">
-              <DatePicker format={dateFormat}/>            
-              <Button.Group classeName="SwitchDay">
-                <Button size="small" type="default">
-                  <Icon type="left" />Hier
-                </Button>
-                <Button size="small" type="default">
-                  Demain<Icon type="right" />
-                </Button>
-              </Button.Group>
-            </div>
+  <div>
+    <div className="Calendar">
+      <DatePicker format={dateFormat}/>            
+      <Button.Group style={styles.buttonGroup}>
+        <Button
+          size="small"
+          type="default"
+          style={styles.button}
+        >
+          <Icon type="left" />Hier
+        </Button>
+        <Button
+          size="small"
+          type="default"
+          style={styles.button}
+        >
+          Demain<Icon type="right" />
+        </Button>
+      </Button.Group>
     </div>
+  </div>
 );
 
 export default HomePage
