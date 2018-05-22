77e010ffaf2e30131c22732aacb41ff2ce4cb2cc
Stephane PERREAUX <stephane.perreaux@free.fr>
2018-05-22 21:48:45 +0200

    Sync update

--- back/routes/sync.js
+++ back/routes/sync.js
@@ -6,16 +6,6 @@ const asyncHandler = require('../services/asyncHandler')
 const fetchIcal = require('../services/fetchIcal')
 const executeQuery2 = require('../services/executeQuery2')
 
-const createTask = rawTask => ({
-  id: rawTask.task_id,
-  name: rawTask.name,
-  projectId: rawTask.project_id,
-  description: rawTask.description,
-  isDiverse: rawTask.isDiverse,
-  isVisible: rawTask.isVisible
-})
-
-const createTasks = rawTasks => rawTasks.map(createTask)
 
 Date.prototype.toMysqlFormat = function() {
   return this.getUTCFullYear() + "-" + twoDigits(1 + this.getUTCMonth()) + "-" + twoDigits(this.getUTCDate()) + " " + twoDigits(this.getHours()) + ":" + twoDigits(this.getUTCMinutes()) + ":" + twoDigits(this.getUTCSeconds());
@@ -53,75 +43,13 @@ const getEventDetailsFromDescription = description => {
   }
 }
 
-const getActivityId = async(activityName) => {
-  const rawRows = await executeQuery2(`SELECT activity_id FROM activity WHERE name='${activityName}'`)
-  const activityId = JSON.parse(JSON.stringify(rawRows))
-  console.log('ACTIVITYID',activityId)
-  if (activityId.length===0)
-    {
-      rawRows = await executeQuery2(`INSERT INTO activity VALUES (NULL, '${activityName}', 0, 0`)
-      rawRows2 = await executeQuery2(`SELECT activity_id FROM activity WHERE name='${activityName}'`)
-    }
-  return activityId
-}
-
-const getProjectId = async(projectName, activityId) => {
-    const rawRows = await executeQuery2(`SELECT project_id FROM project WHERE name='${projectName}' AND activity_id='${activityId}'`)
-    const projectId = JSON.parse(JSON.stringify(rawRows))
-    if (projectId.length===0)
-    {
-      rawRows = await executeQuery2(`INSERT INTO project VALUES (NULL, '${projectName}', '${activityId}', 0, 0`)
-      rawRows2 = await executeQuery2(`SELECT project_id FROM project WHERE name='${projectName}' AND activity_id='${activityId}'`)
-      projectId = JSON.parse(JSON.stringify(rawRows2))
-    }
-    return projectId
-}
-
-const getTaskId = async(taskName, projectId) => {
-    const rawRows = await executeQuery2(`SELECT task_id FROM task WHERE name='${taskName}' AND project_id='${projectId}'`)
-    const taskId = JSON.parse(JSON.stringify(rawRows))
-    if (taskId.length===0)
-    {
-      rawRows = await executeQuery2(`INSERT INTO task VALUES (NULL, '${taskName}', '', '${projectId}', 0, 0, 0`)
-      taskId = JSON.parse(JSON.stringify(rawRows))
-    }
-    return taskId
-}
-
-const checkIsenId = async(isenId, taskId, activityName, projectName, taskName, description, startDate, endDate, username) => {
-    const rawRows = await executeQuery2(`SELECT isen_id FROM event WHERE isen_id='${isenId}'`)
-    const id = JSON.parse(JSON.stringify(rawRows))
-    console.log('id', id)
-    if (id.length===0)
-    {
-      //const duration = 
-      await executeQuery2(`INSERT INTO event VALUES(NULL, '${description}', '0', '0', '${now.toMysqlFormat()}', '${now.toMysqlFormat()}', '${new Date(startDate).toMysqlFormat()}', '${new Date(endDate).toMysqlFormat()}', '${isenId}', '${activityName} - ${projectName} - ${taskName}', '${taskId}', '${req.user.username}', '${duration}')`)
-    }
-    else {
-      const rawRows2 = await executeQuery2(`SELECT isModified FROM event WHERE isen_id='${isenId}`)
-      const isModified = JSON.parse(JSON.stringify(rawRows2))
-      if (isModified[0]===0)
-      {
-        //const duration = 
-        await executeQuery2(`UPDATE event SET isModified='1', startDate='${startDate}', endDate='${endDate}, lastUpdateDate='${now.toMysqlFormat()}', name='${activityName} - ${projectName} - ${taskName}', duration='${duration}' WHERE isen_id='${isenId}' AND username='${username}'`)
-      }
-    }
-}
-
-// créer ou récuperer l'activityId qui correspond à event.activityName
-    // créer ou récuperer le projectId qui correspond à event.projectName
-    // créer ou récuperer le taskId qui correspond à event.taskName
-    //lire isenId de event et regarder s'il n'existe pas dans la base
-    // créer l'event si il existe pas 
-    //sinon
-    // si l'event avec l'isenId === event.isenId a pas isModified à true le mettre à jour (date surtout)
-    // sinon on fait rien
-
 router.get('/api/sync', asyncHandler(async(req, res) => {
   //const { timestamp } = req.params
   // select url from req.user
-  const username=req.user.username
+
   const date1 = new Date()
+
+  const username=req.user.username
   const ical = await fetchIcal(req.query.url)
   const icalKeys = Object.keys(ical)
   const eventKeys = icalKeys.filter(icalKey => icalKey.indexOf('Aurion-') === 0)
@@ -152,7 +80,7 @@ router.get('/api/sync', asyncHandler(async(req, res) => {
     {
       console.log(rawEvent.description)
       return {
-        isenId: eventKey,
+        isenId: eventKey.replace('Aurion-', ''),
         activityName: 'Inconnue',
         projectName: 'Inconnu',
         taskName: 'Inconnue',
@@ -164,10 +92,9 @@ router.get('/api/sync', asyncHandler(async(req, res) => {
     const activityName = description.activityName
     const projectName = description.projectName
     const taskName = description.taskName
-    //2018-05-22T11:30:00.000Z 2018-05-22T13:30:00.000Z
-    console.log(new Date(rawEvent.start), new Date(rawEvent.end))
+
     return {
-      isenId: eventKey,
+      isenId: eventKey.replace('Aurion-', ''),
       activityName,
       projectName,
       taskName,
@@ -178,17 +105,87 @@ router.get('/api/sync', asyncHandler(async(req, res) => {
     
   })
   .filter(event => event !== null)
-  const date2 = new Date()
-  console.log(date2-date1)
   
   for (event of events) {
-    //const activityId = getActivityId(event.activityName)
-    //const projectId = getProjectId(event.projectName, activityId)
-    //const taskId = getTaskId(event.taskName, projectId)
-    //checkIsenId(event.isenId, taskId, event.activityName, event.projectName, event.taskName, event.description, event.startDate, event.endDate, username)
+    await getCurrentEvent(event, username)
+    
+
   }
 
+  const date2 = new Date()
+  console.log(date2-date1)
   res.end()
 }))
+
+const getCurrentEvent = async(event, username) => {
+  const rawRows = await executeQuery2(`SELECT * FROM event WHERE isen_id='${event.isenId}'`)
+  const currentEvent = JSON.parse(JSON.stringify(rawRows))
+  if (currentEvent.length === 0)
+  {
+    await createEvent(event, username)
+    return
+  }
+  const isModifiedEvent = currentEvent.map(event => event.isModified)
+  if (isModifiedEvent[0] === 0)
+  {
+    //updateEvent(event, username)
+    return
+  }
+}
+
+const createEvent = async(event, username) => {
+  let activityId = await getActivityId(event.activityName)
+  if (activityId.length === 0)
+    await createActivity(event.activityName)
+  activityId = await getActivityId(event.activityName)
+
+  let projectId = await getProjectId(event.projectName, activityId)
+  if (projectId.length === 0)
+    await createProject(event.projectName, activityId)
+  projectId = await getProjectId(event.projectName, activityId)
+
+  let taskId = await getTaskId(event.taskName, projectId)
+  if (taskId.length === 0)
+    await createTask(event.taskName, projectId)
+  taskId = await getTaskId(event.taskName, projectId)
+  //Créer evenement 
+  //const duration = 
+  //await executeQuery2(`INSERT INTO event VALUES(NULL, '${event.description}', '0', '0', '${now.toMysqlFormat()}', '${now.toMysqlFormat()}', '${new Date(startDate).toMysqlFormat()}', '${new Date(endDate).toMysqlFormat()}', '${isenId}', '${activityName} - ${projectName} - ${taskName}', '${taskId}', '${req.user.username}', '${duration}')`)
+}
+
+const updateEvent = async(event, username) => {
+  //const duration = 
+  await executeQuery2(`UPDATE event SET isModified='1', startDate='${event.startDate}', endDate='${event.endDate}, lastUpdateDate='${now.toMysqlFormat()}', name='${event.activityName} - ${event.projectName} - ${event.taskName}', duration='${duration}' WHERE isen_id='${event.isenId}' AND username='${username}'`)
+}
+
+const createActivity = async(activityName) => {
+  await executeQuery2(`INSERT INTO activity VALUES (NULL, '${activityName}', 0, 0)`)
+}
+
+const createProject = async(projectName, activityId) => {
+  await executeQuery2(`INSERT INTO project VALUES (NULL, '${projectName}', '${activityId}', 0, 0)`)
+}
+
+const createTask = async(taskName, projectId) => {
+  await executeQuery2(`INSERT INTO task VALUES (NULL, '${taskName}', '', '${projectId}', 0, 0, 0)`)
+}
+
+const getTaskId = async(taskName, projectId) => {
+  const rows = await executeQuery2(`SELECT task_id FROM task WHERE name='${taskName}' AND project_id='${projectId}'`)
+  const taskId = JSON.parse(JSON.stringify(rows))
+  return taskId
+}
+
+const getProjectId = async(projectName, activityId) => {
+  const rows = await executeQuery2(`SELECT project_id FROM project WHERE name='${projectName}' AND activity_id='${activityId}'`)
+  const projectId = JSON.parse(JSON.stringify(rows))
+  return projectId
+}
+
+const getActivityId = async(activityName) => {
+  const rows = await executeQuery2(`SELECT activity_id FROM activity WHERE name='${activityName}'`)
+  const activityId = JSON.parse(JSON.stringify(rows))
+  return activityId
+}
 
 module.exports = router;
